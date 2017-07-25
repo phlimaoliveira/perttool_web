7ac182bb8ed3c2fc3d04a15d5ce2900a9bf2714b
Paulo Henrique <ph.lima.tecnico@gmail.com>
2017-07-25 15:29:23 -0300

    Adding token mismatch expcetion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,5 +88,13 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <script type="text/javascript">
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
 </body>
 </html>
